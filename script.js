3a81859c581bef34df0c9bbf3b3196e633adfa56
Loupdo <188009481+Loupdo@users.noreply.github.com>
2024-12-12 04:53:30 -0500

    added human choice and play round TODO play game

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const getComputerChoice = () => {
     const randomNumber= Math.floor(3*(Math.random()))
     if (randomNumber <1){
-        return "Scissors"
+        return "scissors"
     } else if (randomNumber <2){
-        return "Paper"
+        return "paper"
     } else {
-        return "Rock"
+        return "rock"
     }
 }
 
 const getHumanChoice = () => {
-    
+    const HumanChoice = prompt("Enter either Rock, Paper, or Scissors:")
+    return HumanChoice.toLowerCase()
 }
 
+const playRound = (humanChoice, computerChoice) =>{
+if((humanChoice === "rock" && computerChoice === "scissors")||(humanChoice === "scissors" && computerChoice === "paper")||(humanChoice === "paper" && computerChoice === "rock")){
+    console.log(`You won!! ${humanChoice} beats ${computerChoice}`) 
+    humanScore++
+} else if ((humanChoice === "paper" && computerChoice === "scissors")||(humanChoice === "rock" && computerChoice === "paper")||(humanChoice === "scissors" && computerChoice === "rock")){
+    console.log(`You lost!! ${humanChoice} is beaten by ${computerChoice}`) 
+    computerScore++
+} else {
+    console.log(`You both picked ${humanChoice}. Score unchanged`)
+}
+console.log(`\nComputer:${computerScore} vs you:${humanScore}`)
 
-console.log(getComputerChoice())
+
+}
+let humanScore = 0
+let computerScore = 0
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(humanSelection)
+playRound(humanSelection,computerSelection)
